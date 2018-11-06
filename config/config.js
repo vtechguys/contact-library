dbf7b1c457d9e702583ccf0c35b09b1b9564fd18
= <=>
2018-11-06 20:43:18 +0530

    gitignores

--- config/config.js
+++ config/config.js
@@ -1,19 +1,6 @@
-module.exports = {
-    SMTP_SERVICE:"gmail",
-    SMTP_MAIL_ID:"dev.devopsgenesis@gmail.com",
-    SMTP_MAIL_PASS:"Ytrewq.12345",
-
-
-    COMPANY_NAME:"DevOps Genesis",
-
-    DB_URL:'mongodb://localhost:27017/email',
-
-    VALID_MAIL_TYPES:['query','followUp','newsLetter','promotionalOffer'],
-    ADMIN_MAILS:[
-        {mail:'aniketjhaindia@gmail.com',pass:"abcd"},
-        {mail:'ashughildiyal5@gmail.com',pass:"abcd"},
-        {mail:'mishraprince0786@gmail.com',pass:"abcd"},
-        {mail:'aloksingh3112@gmail.com',pass:"abcd"},
-        {mail:'hsharma151197@gmail.com',pass:"abcd"}]
-    
+if(process.env.NODE_ENV==="production"){
+    module.exports=require('./config_prod')
+}
+else{
+    module.exports=require('./config_dev');
 }
