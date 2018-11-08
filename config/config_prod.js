249507496f04c61988acf1f4cf2e86a12eb4ab21
= <=>
2018-11-08 21:43:28 +0530

    heroku deploying

--- config/config_prod.js
+++ config/config_prod.js
@@ -10,10 +10,10 @@ module.exports = {
 
     VALID_MAIL_TYPES:['query','followUp','newsLetter','promotionalOffer'],
     ADMIN_MAILS:[
-        {mail:process.env.USER_1_ID,pass:process.env.USER_1_PASS},
-        {mail:process.env.USER_2_ID,pass:process.env.USER_2_PASS},
-        {mail:process.env.USER_3_ID,pass:process.env.USER_3_PASS},
-        {mail:process.env.USER_4_ID,pass:process.env.USER_4_PASS},
-        {mail:process.env.USER_5_ID,pass:process.env.USER_5_PASS}]
+        {mail:process.env.USER_1_ID,pass:process.env.USER_PASS},
+        {mail:process.env.USER_2_ID,pass:process.env.USER_PASS},
+        {mail:process.env.USER_3_ID,pass:process.env.USER_PASS},
+        {mail:process.env.USER_4_ID,pass:process.env.USER_PASS},
+        {mail:process.env.USER_5_ID,pass:process.env.USER_PASS}]
     
 }
